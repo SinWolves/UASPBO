de8dc70912bcd5b0f480daf3c8275714ae0c3962
PatrickNW <patrickdwangsa@gmail.com>
2025-06-16 14:28:20 +0700

    more working admin form

--- pbo/src/main/java/com/uas/pbo/AdminMainController.java
+++ pbo/src/main/java/com/uas/pbo/AdminMainController.java
@@ -1,24 +1,30 @@
 package com.uas.pbo;
 
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.uas.pbo.model.User;
+
 @Controller
 public class AdminMainController {
     @GetMapping("adminHome")
-    public String home(Model model) {
+    public String home(@AuthenticationPrincipal User user, Model model) {
+        model.addAttribute("name", user.getName());
         return "admin/home"; 
     }
 
     @GetMapping("adminClass-list")
-    public String classList(Model model) {
+    public String classList(@AuthenticationPrincipal User user, Model model) {
+        model.addAttribute("name", user.getName());
         return "admin/Class_list"; 
     }
 
     @GetMapping("adminMahasiswa")
-    public String mahasiswa(Model model) {
+    public String mahasiswa(@AuthenticationPrincipal User user, Model model) {
+        model.addAttribute("name", user.getName());
         return "admin/mahasiswa"; 
     }
 }
