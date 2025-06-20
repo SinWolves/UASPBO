c5423d5aa456c6944f973d0e9958d669133ece40
PatrickNW <patrickdwangsa@gmail.com>
2025-06-20 16:09:28 +0700

    FIX ISSUE KECIL

--- pbo/src/main/java/com/uas/pbo/repository/MahasiswaRepository.java
+++ pbo/src/main/java/com/uas/pbo/repository/MahasiswaRepository.java
@@ -3,7 +3,7 @@ package com.uas.pbo.repository;
 import com.uas.pbo.model.Mahasiswa;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MahasiswaRepository extends JpaRepository<Mahasiswa, String> {
+public interface MahasiswaRepository extends JpaRepository<Mahasiswa, Long> {
 
     // Custom method to check if an enrollment already exists to prevent duplicates
     boolean existsByNimAndCourseCode(String nim, String courseCode);
